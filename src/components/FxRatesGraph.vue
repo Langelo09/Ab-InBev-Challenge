c452f18f08a0c6ad3f4f622838e5b20407097e23
Aflt <angelfernando.luna@bbva.com>
2022-03-06 14:31:07 -0600

    Add FxRatesGraph

--- src/components/FxRatesGraph.vue
+++ src/components/FxRatesGraph.vue
@@ -30,8 +30,8 @@
 
       <v-col cols="12">
         <v-flex xs12 sm6 offset-sm3>
-            graph content
-          </v-flex>
+          <div id="fxRatesGraph" style="width: 100%; height: 65vh;"></div>
+        </v-flex>
       </v-col>
     </v-row>
 
@@ -39,13 +39,84 @@
 </template>
 
 <script>
+import * as echarts from 'echarts/core'
+import { LineChart } from 'echarts/charts'
+import { UniversalTransition } from 'echarts/features'
+import { CanvasRenderer, SVGRenderer } from 'echarts/renderers'
+import {
+  TitleComponent,
+  TooltipComponent,
+  LegendComponent,
+  MarkLineComponent,
+  GridComponent
+} from 'echarts/components'
+
+echarts.use([
+  TitleComponent,
+  TooltipComponent,
+  LineChart,
+  CanvasRenderer,
+  SVGRenderer,
+  UniversalTransition,
+  LegendComponent,
+  MarkLineComponent,
+  GridComponent
+])
+
   export default {
     name: 'FxRatesGraph',
     data: () => ({
       date: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
       menu: false,
-      modal: false
+      modal: false,
+      currencies: ['MXN', 'USD', 'AUD', 'CAD', 'PLN', 'NZD', 'CHF', 'GBP', 'DOP', 'EUR'],
+      currenciesValue: [120,120,120,150,190,489,471,100,160,230]
     }),
+    mounted () {
+      this.init()
+    },
+    methods: {
+      init() {
+        // initialize the echarts instance
+        let myChart = echarts.init(
+          document.getElementById('fxRatesGraph')
+        )
+        // Draw the chart
+        const option = {
+          title: {
+            text: 'Fx Rates'
+          },
+          tooltip: {
+            trigger: 'axis',
+            show: true
+          },
+          xAxis: [
+            {
+              type: 'category',
+              boundaryGap: false,
+              name: 'Currencies',
+              data: this.currencies
+            }
+          ],
+          yAxis: [
+            {
+              type: 'value',
+              name: 'Fx'
+            }
+          ],
+          series: [{
+              data: this.currenciesValue,
+              type: 'line'
+          }]
+        }
+
+        myChart.setOption(option)
+
+        window.addEventListener('resize', function () {
+          myChart.resize()
+        })
+      }
+    },
     watch: {
       date(newValue) {
         console.log(newValue)
@@ -56,6 +127,6 @@
 
 <style lang="scss" scoped>
 .v-text-field{
-      width: 200px;
+  width: 200px;
 }
 </style>
